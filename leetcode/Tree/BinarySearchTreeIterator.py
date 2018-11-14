b3a403400a50e161dc0f95851889ba939fdbd9ea
Jinx19 <472580488@qq.com>
2018-11-14 23:46:45 +0800

    二叉搜索树 迭代器

---
+++ leetcode/Tree/BinarySearchTreeIterator.py
@@ -0,0 +1,64 @@
+# Definition for a  binary tree node
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+from collections import deque
+
+from leetcode.Tree.TreeNode import TreeNode
+
+
+class BSTIterator(object):
+    root = None
+
+    def __init__(self, root):
+        """
+        :type root: TreeNode
+        """
+        self.root = root
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        return self.root
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        q = deque()
+        temp = self.root
+
+        # 找到左下角的结点，即最小点
+        while temp:
+            q.append(temp)
+            temp = temp.left
+
+        smallest = q.pop()
+
+        if len(q) > 0:
+            parent = q.pop()
+            parent.left = smallest.right
+        else:
+            self.root = smallest.right
+
+        return smallest.val
+
+
+# root = TreeNode(8)
+# root.left = TreeNode(5)
+# root.left.left = TreeNode(3)
+# root.left.right = TreeNode(6)
+# root.right = TreeNode(10)
+# root.right.left = TreeNode(9)
+#
+# i, v = BSTIterator(root), []
+# while i.hasNext():
+#     v.append(i.next())
+#
+# print(v)
+q = deque()
+print(q.pop())
